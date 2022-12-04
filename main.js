6426d494ca711517bedc2ef5a79374fd9422bfe4
MeruyertBizhanova <meruyert.bizhanova@nu.edu.kz>
2022-12-04 23:42:26 +0600

    final commit

--- main.js
+++ main.js
@@ -109,6 +109,22 @@ let swiper = new Swiper('.mySwiper', {
 	
 });
 
+//skills
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader =document.querySelectorAll('.skills__header')
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+    
+    for(i=0; i<skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close'}
+   
+    if(itemClass ==='skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+skillsHeader.forEach((el)=>{
+    el.addEventListener('click', toggleSkills)
+})
 
 sr.reveal('.home-text',{delay:350, origin:'left'})
 sr.reveal('.home-img',{delay:350, origin:'right'})
